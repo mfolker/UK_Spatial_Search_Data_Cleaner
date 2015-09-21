5de5e0053eda1654b392faa23f0f8f5bd47c28ca
Matthew Folker <matthew.folker@gmail.com>
2015-09-21 23:05:48 +0100

    PostcodeWktTest

--- UKSSDC/UKSSDC.Tests/PostcodesTests.cs
+++ UKSSDC/UKSSDC.Tests/PostcodesTests.cs
@@ -1,11 +1,43 @@
 ﻿using System;
+using System.Data.Entity.Spatial;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using UKSSDC.Services.Data;
+using UKSSDC.Services.Import;
 
 namespace UKSSDC.Tests
 {
     [TestClass]
     public class PostcodesTests
     {
-        //TODO: Test method for PostcodeWkt
+        [TestMethod]
+        public void PostcodeWktTest()
+        {
+            //Arrange
+
+            IUnitOfWork uowPR = new UnitOfWork();
+            ICsvReader csvReaderPR = new CsvReader(uowPR);
+            IProgressReporter progressReporter = new ProgressReporter(uowPR, csvReaderPR);
+
+            IUnitOfWork uowPostcodes = new UnitOfWork();
+            ICsvReader csvReaderPostcodes = new CsvReader(uowPR);
+
+            Postcodes postcodes = new Postcodes(csvReaderPostcodes, progressReporter, uowPostcodes);
+            PrivateObject postcodesPrivateObject = new PrivateObject(postcodes);
+
+            //Easting Northings for AB101YN
+            int easting = 392564; // X Co-ordinate, Longitiude
+            int northing = 805767; // Y Co-ordinate, Latitude
+
+            object[] param = {easting, northing}; 
+
+            //Act
+
+            DbGeography AB101YN = (DbGeography) postcodesPrivateObject.Invoke("PostcodeWkt", param); 
+
+            //Assert
+
+            Assert.IsTrue(AB101YN.Latitude == -2.12451000395 && AB101YN.Longitude == 57.1427363893);
+        }
     }
 }
